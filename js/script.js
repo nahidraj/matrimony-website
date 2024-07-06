8fe40aa9a0f878be6c95180d066aa74df8eb0366
Khairul Alam Nahid <123856849+nahidraj@users.noreply.github.com>
2024-07-06 22:18:38 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,138 @@
+$(function () {
+  "use strict";
+
+  $('.slider').slick({
+    draggable: false,
+    arrows: false,
+    dots: false,
+    fade: true,
+    autoplay: true,
+    speed: 800,
+    autoplaySpeed: 8000,
+    infinite: true,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+    prevArrow: '<i class="fas left icon fa-chevron-left"></i>',
+    nextArrow: '<i class="fas right icon fa-chevron-right"></i>',
+  })
+
+  $('.profile_slider').slick({
+    draggable: true,
+    arrows: true,
+    dots: false,
+    autoplay: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 600,
+    autoplaySpeed: 3000,
+    infinite: true,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+    prevArrow: '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon arrow arrow_left icon-tabler icons-tabler-outline icon-tabler-arrow-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l6 6" /><path d="M5 12l6 -6" /></svg>',
+    nextArrow: '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon arrow arrow_right icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg>',
+  })
+  // chosen js
+  $(".chosen").chosen()
+
+
+  // testimonial slider js
+  $(".testimonial_slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 600,
+    arrows: false,
+    dots: false,
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+      }
+    }]
+  });
+
+  // magnific video js
+  $(".vidplay").magnificPopup({
+    type: "iframe",
+    iframe: {
+      markup: '<div class="mfp-iframe-scaler">' +
+        '<div class="mfp-close"></div>' +
+        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+        "</div>",
+      patterns: {
+        youtube: {
+          index: "youtube.com/",
+          id: "v=",
+          src: "https://www.youtube.com/embed/%id%?autoplay=1",
+        },
+      },
+      srcAction: "iframe_src",
+    },
+  });
+
+
+  // Fixed menu js start
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 100) {
+      $("#sticky-header").removeClass("sticky-menu");
+      $("#header-fixed-height").removeClass("active-height");
+    } else {
+      $("#sticky-header").addClass("sticky-menu");
+      $("#header-fixed-height").addClass("active-height");
+    }
+  });
+
+  // // gallery popup js
+  // $(".parent-container").magnificPopup({
+  //   delegate: "a",
+  //   type: "image",
+  //   gallery: {
+  //     enabled: true,
+  //   },
+  // });
+
+  // $(".js-select2").select2({
+  //   closeOnSelect: true,
+  // });
+
+  // // scroll to top js
+  var btn = $(".scroll-to-top");
+
+  $(window).scroll(function () {
+    btn.toggleClass("show", $(window).scrollTop() > 300);
+  });
+
+  btn.click(function (e) {
+    e.preventDefault();
+    if (navigator.userAgent.toLowerCase().indexOf("firefox") > -1) {
+      $("html").animate({
+          scrollTop: 0,
+        },
+        800
+      );
+    } else {
+      $("html, body").animate({
+          scrollTop: 0,
+        },
+        0
+      );
+    }
+  });
+
+  // Mobile menu js start
+
+  $(".mobile-topbar .bars").on("click", function () {
+    $(".mobile-menu-overlay,.mobile-menu-main").addClass("active");
+    return false;
+  });
+
+  $(".close-mobile-menu,.mobile-menu-overlay").on("click", function () {
+    $(".mobile-menu-overlay,.mobile-menu-main").removeClass("active");
+  });
+
+  $(".sub-mobile-menu ul").hide();
+  $(".sub-mobile-menu a").on("click", function () {
+    $(this).parent(".sub-mobile-menu").children("ul").slideToggle("100");
+    $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+  });
+});
